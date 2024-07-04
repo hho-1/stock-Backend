b6bf329540562b530ab170cf7e96a4eaebde469c
Habip Hakkioglu <h.hakkioglu@icloud.com>
2024-07-04 19:49:38 +0200

    userschema

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,8 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- *
 {
     "username": "admin",
@@ -37,108 +37,110 @@ const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
 // User Model:
 
-const UserSchema = new mongoose.Schema({
-
+const UserSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true,
-        index: true
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      index: true,
     },
 
     password: {
-        type: String,
-        trim: true,
-        required: true
+      type: String,
+      trim: true,
+      required: true,
     },
 
     email: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true,
-        index: true
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      index: true,
     },
 
     first_name: {
-        type: String,
-        trim: true,
-        required: true
+      type: String,
+      trim: true,
+      required: true,
     },
 
     last_name: {
-        type: String,
-        trim: true,
-        required: true
+      type: String,
+      trim: true,
+      required: true,
     },
 
     is_active: {
-        type: Boolean,
-        default: true
+      type: Boolean,
+      default: true,
     },
 
     is_staff: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
 
     is_superadmin: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
-
-}, { collection: 'users', timestamps: true })
+  },
+  { collection: "users", timestamps: true }
+);
 
 /* ------------------------------------------------------- */
 // Schema Configs:
 
-const passwordEncrypt = require('../helpers/passwordEncrypt')
+const passwordEncrypt = require("../helpers/passwordEncrypt");
 
-UserSchema.pre(['save', 'updateOne'], function (next) {
+UserSchema.pre(["save", "updateOne"], function (next) {
+  // get data from "this" when create;
+  // if process is updateOne, data will receive in "this._update"
+  const data = this?._update || this;
 
-    // get data from "this" when create;
-    // if process is updateOne, data will receive in "this._update"
-    const data = this?._update || this
+  // email@domain.com
+  const isEmailValidated = data.email
+    ? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email) // test from "data".
+    : true;
 
-    console.log(data)
+  if (isEmailValidated) {
+    if (data?.password) {
+      // pass == (min 1: lowerCase, upperCase, Numeric, @$!%*?& + min 8 chars)
+      const isPasswordValidated =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(
+          data.password
+        );
 
-    // email@domain.com
-    const isEmailValidated = data.email
-        ? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email) // test from "data".
-        : true
+      console.log("Password validation result:", isPasswordValidated); // Debugging line
 
-    if (isEmailValidated) {
+      if (isPasswordValidated) {
+        this.password = data.password = passwordEncrypt(data.password);
+        this._update = data; // updateOne will wait data from "this._update".
+        console.log(
+          "Password encrypted and data updated:",
+          this.password,
+          this._update
+        ); // Debugging line
+      } else {
+        console.log("Password validation failed for:", data.password); // Debugging line
+        next(new Error("Password not validated."));
+      }
 
-        if (data?.password) {
-
-            // pass == (min 1: lowerCase, upperCase, Numeric, @$!%*?& + min 8 chars)
-            const isPasswordValidated = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*+?&]).{8,}$/.test(data.password)
-
-            if (isPasswordValidated) {
-
-                this.password = data.password // = passwordEncrypt(data.password)     Bu kismi deployment öncesi kaldirdim mecburen
-                this._update = data // updateOne will wait data from "this._update".
-
-            } else {
-
-                next(new Error('Password not validated.'))
-            }
-        }
-
-        next() // Allow to save.
-
-    } else {
-
-        next(new Error('Email not validated.'))
     }
-})
+
+    next(); // Allow to save.
+  } else {
+    next(new Error("Email not validated."));
+  }
+});
 /* ------------------------------------------------------- */
 // FOR REACT PROJECT:
-UserSchema.pre('init', function (data) {
-
-    data.id = data._id
-    data.createds = data.createdAt.toLocaleDateString('tr-tr')
-})
+UserSchema.pre("init", function (data) {
+  data.id = data._id;
+  data.createds = data.createdAt.toLocaleDateString("tr-tr");
+});
 /* ------------------------------------------------------- */
-module.exports = mongoose.model('User', UserSchema)
+module.exports = mongoose.model("User", UserSchema);
